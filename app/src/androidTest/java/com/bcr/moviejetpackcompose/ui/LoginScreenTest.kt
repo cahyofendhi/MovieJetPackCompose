781fcd596568f62a2776fc6733158de0d8e4fc4e
Cahyo Fendhi <cahyofendhi@gmail.com>
2022-03-01 12:24:50 +0700

    enhancement ui test login

--- app/src/androidTest/java/com/bcr/moviejetpackcompose/ui/LoginScreenTest.kt
+++ app/src/androidTest/java/com/bcr/moviejetpackcompose/ui/LoginScreenTest.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
@@ -19,13 +18,17 @@ import com.bcr.moviejetpackcompose.ui.home.HomeScreen
 import com.bcr.moviejetpackcompose.ui.login.LoginScreen
 import com.bcr.moviejetpackcompose.ui.theme.AppTheme
 import com.bcr.moviejetpackcompose.ui.tv.TVScreen
+import com.bcr.moviejetpackcompose.utils.DIALOG_LOADING_TAG
 import com.bcr.moviejetpackcompose.utils.EMAIL_FIELD_LOGIN_TAG
 import com.bcr.moviejetpackcompose.utils.PASSWORD_FIELD_LOGIN_TAG
 import com.bcr.moviejetpackcompose.utils.SUBMIT_BUTTON_LOGIN_TAG
 import com.google.accompanist.pager.ExperimentalPagerApi
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.*
+import kotlin.concurrent.schedule
 
 @RunWith(AndroidJUnit4::class)
 class LoginScreenTest {
@@ -34,14 +37,13 @@ class LoginScreenTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule(MainActivity::class.java)
 
-    private lateinit var navController: NavHostController
+    private lateinit var appState: MovieAppState
 
     @OptIn(ExperimentalPagerApi::class)
     private fun launchLoginScreenWithNavGraph() {
         composeTestRule.setContent {
             AppTheme {
-                val appState = rememberMovieAppState()
-                navController = appState.navController
+                appState = rememberMovieAppState()
                 Scaffold(
                     scaffoldState = appState.scaffoldState,
                     modifier = Modifier.background(Color.White),
@@ -93,11 +95,42 @@ class LoginScreenTest {
             onNodeWithTag(SUBMIT_BUTTON_LOGIN_TAG)
                 .assertIsEnabled()
 
+            onNodeWithTag(SUBMIT_BUTTON_LOGIN_TAG)
+                .performClick()
+
+            waitForIdle()
+
+            onNodeWithTag(DIALOG_LOADING_TAG)
+                .assertIsDisplayed()
+
+            asyncTimer(3000)
+
+            val route = appState.navController.currentDestination?.route
+            Assert.assertEquals(route, HomeSections.Home.route)
+
         }
+    }
+
+    @OptIn(ExperimentalPagerApi::class)
+    fun asyncTimer (delay: Long = 1000) {
+        AsyncTimer.start (delay)
+        composeTestRule.waitUntil (
+            condition = {AsyncTimer.expired},
+            timeoutMillis = delay + 1000
+        )
     }
 
 }
 
+object AsyncTimer {
+    var expired = false
+    fun start(delay: Long = 1000){
+        expired = false
+        Timer().schedule(delay) {
+            expired = true
+        }
+    }
+}
 
 @ExperimentalPagerApi
 @Composable
