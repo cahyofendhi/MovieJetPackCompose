cadf34da3c5f9997cb39199bdebf5f1490e87200
Bekti Cahyo <cahyofendhi@gmail.com>
2022-12-12 22:05:21 +0700

    add appcenter and change java version

--- app/src/main/java/com/bcr/moviejetpackcompose/ui/MainActivity.kt
+++ app/src/main/java/com/bcr/moviejetpackcompose/ui/MainActivity.kt
@@ -5,14 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.bcr.moviejetpackcompose.ui.app.MovieApp
 import com.google.accompanist.pager.ExperimentalPagerApi
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 @ExperimentalPagerApi
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupAppCenter()
         setContent {
             MovieApp()
         }
+    }
+
+    private fun setupAppCenter() {
+        AppCenter.start(
+            application, "891a39dc-042f-44bd-a20f-9feb3a9f2730",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
